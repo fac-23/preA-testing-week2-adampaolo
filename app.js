1a38905f56b36741e05f015c836918007fd4fcb0
Paolo <paolo.ghidoni82@gmail.com>
2021-11-28 10:58:30 +0000

    Add checkTaskOff() function and test - Relates #5 #9
    * Added checkTaskOff() function, at the moment it only toggles
    the "completed" task, which should already be enough to filter them,
    I would like it also to change the value of the "completed" key in the
    note object that we store in the notes array and also give more visual feedback, will work on it!
    
    * Added 2 tests to test the checkTaskOff() function.

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
   VARIABLES
 \*-------------------------------------------*/
 
+/* input form */
 const form = document.querySelector(".note__form");
+const noteCloseBtn = document.querySelector("#note__btn--close");
 const noteTitle = document.querySelector("#note__title");
 const noteBody = document.querySelector("#note__body");
 const noteSubmitBtn = document.querySelector("#note__btn--submit");
-const noteCloseBtn = document.querySelector("#note__btn--close");
+
+/* note list */
 const tasksList = document.querySelector(".tasks__list");
+const checkTaskOffBtns = document.querySelectorAll(".tasks__note-check");
 let notes = [];
 
 
@@ -15,6 +19,7 @@ let notes = [];
 /*-------------------------------------------*\
   FUNCTIONS
 \*-------------------------------------------*/
+
 // Generate note when user clicks button or presses enter on keyboard
 function generateNote() {
   const formData = new FormData(form);
@@ -30,50 +35,47 @@ function generateNote() {
 }
 
 
-// Create the html note structure, Update the DOM and add the note to the notes array
+// Function to create the html note structure, Update the DOM and add the note to the notes array
 function addNote(titleText, bodyText) {
 
   // create the note to update the DOM
   let note = document.createElement("li");
-  note.classList.add("tasks__note");
-  note.id = notes.length;
+  note.classList.add("tasks__note", "stack-md");
+  note.id = "note_" + notes.length;
 
-  // create note check button
-  let noteCheck = document.createElement("input");
-  noteCheck.type = "checkbox";
-  noteCheck.id = "tasks__note-check";
-  noteCheck.name = "tasks__note-check";
-  noteCheck.value = "notecheck";
-  note.prepend(noteCheck);
-
-  // create note title
-  let noteH2 = document.createElement("h2");
-  noteH2.classList.add("tasks__note-title");
-  noteH2.textContent = titleText;
-  note.append(noteH2);
-
-  // create note body
-  let noteContent = document.createElement("div");
-  noteContent.classList.add("tasks__note-body");
-  noteContent.textContent = bodyText;
-  note.append(noteContent);
-
+  // Dinamically create html note content
+  note.innerHTML = `
+  <button type="button" class="tasks__note-check">☑️</button>
+  <h2 class='tasks__note-title'>${titleText}</h2>
+  <p class='tasks__note-body'>${bodyText}</p>
+  `
+  
   // update the DOM
   tasksList.append(note);
 
   // store the note into an object and push it to the notes array
   if(noteTitle.value !== '') {
     notes.push({
-      "noteId": notes.length,
+      "noteId": "note_" + notes.length,
       "noteTitle": titleText,
-      "noteBody": bodyText,
+      "noteBody": bodyText, 
       "completed": false
     });
   }
-  console.log(noteTitle.value);
+  // console.log(noteTitle.value);
 
   console.log(notes);
+}
 
+
+// Function to check task off
+function checkTaskOff(e) {
+  // console.log(typeof e);
+  if(e.target.classList.contains("tasks__note-check")) {
+    let checkedBtnTarget = e.target;
+    checkedBtnTarget.parentElement.classList.toggle('completed');
+  }
+  // return e; // for testing checkTaskOff receives an object
 }
 
 
@@ -90,6 +92,11 @@ window.addEventListener("keydown", (e) => {
 
 // When user clicks new note button, get the submitted data from the form, then call addNote();
 form.addEventListener("submit", (e) => {
-  e.preventDefault() 
+  e.preventDefault();
   generateNote();
 })
+
+
+// When user clicks on checkTaskOff button, the note turns green
+tasksList.addEventListener("click", checkTaskOff);
+
