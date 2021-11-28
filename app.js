dc5385d2bc55a97b50a3b6f60e0ddeee4f2634f2
Paolo <paolo.ghidoni82@gmail.com>
2021-11-28 08:00:15 +0000

    Update add note function - Relates #4
    * Created new generateNote() function to pass in both event handlers
    (when user clicks submit button and when user presses 'enter' on
    keyboard). The function gets the user input using formData.get(), calls
    addNote() and then resets the user input fields.
    * Note title field is now required
    * Notes only get added to the notes array if a value is entered in
    the note title field
    * User can enter a note pressing 'enter' key on keyboard, only if
    the textarea field is not active ('enter' serves as 'new line' when
    textarea is active)
    * tidied up code

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-// Variable
+/*-------------------------------------------*\
+  VARIABLES
+\*-------------------------------------------*/
+
 const form = document.querySelector(".note__form");
 const noteTitle = document.querySelector("#note__title");
 const noteBody = document.querySelector("#note__body");
@@ -9,54 +12,84 @@ let notes = [];
 
 
 
-// Functions
+/*-------------------------------------------*\
+  FUNCTIONS
+\*-------------------------------------------*/
+// Generate note when user clicks button or presses enter on keyboard
+function generateNote() {
+  const formData = new FormData(form);
 
-function addNote() {
-  const titleText = noteTitle.value;
-  const bodyText = noteBody.value;
+  // Retrieve values entered by user
+  const title = formData.get("note__title");
+  const body = formData.get("note__body");
+  addNote(title, body);
 
-  notes.push({
-    'noteId': notes.length,
-    'noteTitle': titleText,
-    'noteBody': bodyText,
-    'completed': false
-  })
-
-  console.log(titleText);
+  // reset form
+  noteTitle.value = '';
+  noteBody.value = '';
+}
 
 
-  let note = document.createElement('li');
-  note.classList.add('tasks__note');
+// Create the html note structure, Update the DOM and add the note to the notes array
+function addNote(titleText, bodyText) {
+
+  // create the note to update the DOM
+  let note = document.createElement("li");
+  note.classList.add("tasks__note");
   note.id = notes.length;
 
-  let noteCheck = document.createElement('input');
-  noteCheck.type = 'checkbox';
-  noteCheck.id = 'tasks__note-check';
-  noteCheck.name = 'tasks__note-check';
-  noteCheck.value = 'notecheck';
+  // create note check button
+  let noteCheck = document.createElement("input");
+  noteCheck.type = "checkbox";
+  noteCheck.id = "tasks__note-check";
+  noteCheck.name = "tasks__note-check";
+  noteCheck.value = "notecheck";
   note.prepend(noteCheck);
 
-  let noteH2 = document.createElement('h2');
-  noteH2.classList.add('tasks__note-title');
+  // create note title
+  let noteH2 = document.createElement("h2");
+  noteH2.classList.add("tasks__note-title");
   noteH2.textContent = titleText;
   note.append(noteH2);
 
-  let noteContent = document.createElement('div');
+  // create note body
+  let noteContent = document.createElement("div");
   noteContent.classList.add("tasks__note-body");
   noteContent.textContent = bodyText;
   note.append(noteContent);
 
-
+  // update the DOM
   tasksList.append(note);
+
+  // store the note into an object and push it to the notes array
+  if(noteTitle.value !== '') {
+    notes.push({
+      "noteId": notes.length,
+      "noteTitle": titleText,
+      "noteBody": bodyText,
+      "completed": false
+    });
+  }
+  console.log(noteTitle.value);
+
+  console.log(notes);
+
 }
 
 
+/*-------------------------------------------*\
+  EVENT LISTENERS
+\*-------------------------------------------*/
 
-// Event listeners
+// When user presses enter on keyboard, get the submitted data from the form, then call addNote();
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && noteBody !== document.activeElement) { // textarea must not be active
+    generateNote();
+  }
+})
 
-noteSubmitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  addNote();
-});
-
-
+// When user clicks new note button, get the submitted data from the form, then call addNote();
+form.addEventListener("submit", (e) => {
+  e.preventDefault() 
+  generateNote();
+})
