c6969f1c1644645f4779e4142bd92986ebd4b52c
Paolo <paolo.ghidoni82@gmail.com>
2021-12-03 14:56:13 +0000

    Fix vulnerability issue
    Hi Adam! I realised our code was potentially open to Cross-Site Scripting threats.
    I worked on it and instead of creating a new note using just a template literal, the title and body are now generated separately and the user's input is passed in as text using the textContent property to prevent attacks 👾 👍🏻
    Just in case we want to use the app with a database..
    Let me know if I made mistakes and if it makes sense to you too! 🙌🏻

--- app.js
+++ app.js
@@ -47,22 +47,36 @@ function addNote() {
   note.classList.add("tasks__note");
   note.id = "note_" + notes.length;
 
-  // dinamically create html note content
+
+  // create basic html note structure
   note.innerHTML = `
-  <div class="tasks__note-content">
-    <h2 class='tasks__note-title'>${title}</h2>
-    <p class='tasks__note-body'>${body}</p>
-  </div>
+  
   <div class="tasks__note-controls">
     <input type="checkbox" class="btn btn-control tasks__note-check" aria-label="Click this button to check task as completed"></input>
     <button  type="button" class="btn btn-control tasks__note-delete" aria-label="Click this button to delete task">X</button>
   </div>
   `
-  
+
+  // Add user input to the note using the textContent property to prevent Cross-Site Scripting vulnerability.
+  const noteContent = document.createElement("div");
+  noteContent.classList.add("tasks__note-content");
+
+  const noteTitle = document.createElement("h2");
+  noteTitle.classList.add("tasks__note-title");
+  noteTitle.textContent = title;
+
+  const noteBody = document.createElement("p");
+  noteBody.classList.add("tasks__note-body");
+  noteBody.textContent = body;
+
+  noteContent.append(noteTitle, noteBody);
+  note.prepend(noteContent);
+
+
   // update the DOM
   tasksList.append(note);
 
-  // store the note into an object and push it to the notes array
+  // if the title field is not empty, store the note into an object and push it to the notes array
   if (noteTitle.value !== '') {
     notes.push({
       "noteId": "note_" + notes.length,
@@ -76,6 +90,7 @@ function addNote() {
   form.reset();
   // console.log(notes);
 
+  // let impaired users know the enter new note form section is collapsed
   newNoteBtn.setAttribute('aria-expanded', 'false');
 
 }
@@ -105,7 +120,6 @@ function taskDelete(e) {
   if (e.target.classList.contains("tasks__note-delete")) {
     let deleteBtn = e.target;
     deleteBtn.closest(".tasks__note").remove();
-    // classList.add('hidden');
   }
 }
 
@@ -122,10 +136,15 @@ function showNoteForm(e) {
 
 // function to filter out completed tasks
 function filterCompleted() {
+
+  // retrieve an HTML collection of all notes rendered in the DOM and turn it into an array 
   let notesDisplayed = Array.from(tasksList.children);
+
+  // filter the array and only keep the completed tasks
   let completedTasks = notesDisplayed.filter(el => {
 
-    let checkbox = el.childNodes[3].firstElementChild;
+    // check if the checkbox of each task is checked
+    let checkbox = el.lastElementChild.firstElementChild;
     return checkbox.checked;
 
   });
@@ -172,3 +191,33 @@ newNoteBtn.addEventListener("click", showNoteForm);
 
 // when filter completed button is clicked, the tasks are filtered
 filterBtn.addEventListener("click", filterCompleted);
+
+
+
+
+
+
+/*-------------------------------------------*\
+  discarded - still interesting - code
+\*-------------------------------------------*/
+
+
+/* longer procedure to generate html elements of the note */
+
+  // const noteControls = document.createElement("div");
+  // noteControls.classList.add("tasks__note-controls");
+
+  // const noteCheck = document.createElement("input");
+  // noteCheck.setAttribute("type", "checkbox");
+  // noteCheck.classList.add("btn", "btn-control", "tasks__note-check");
+  // noteCheck.setAttribute("aria-label", "Click this button to check task as completed");
+
+  // const noteDelete = document.createElement("button");
+  // noteDelete.setAttribute("type", "button");
+  // noteDelete.classList.add("btn", "btn-control", "tasks__note-delete");
+  // noteDelete.setAttribute("aria-label", "Click this button to delete task");
+  // noteDelete.textContent = "X";
+
+
+  // noteControls.append(noteCheck, noteDelete);
+  // note.append(noteControls);
